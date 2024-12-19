cfd6792b831916ee839d29698955cbc7f6c07948
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2024-12-19 08:47:56 -0300

    Create database

--- AnimalManager.API/AnimalManager.API/Program.cs
+++ AnimalManager.API/AnimalManager.API/Program.cs
@@ -1,9 +1,29 @@
+using AnimalManager.Application.Interfaces;
+using AnimalManager.Application.Services;
+using AnimalManager.Application.Validators;
+using AnimalManager.Domain.Interfaces;
+using AnimalManager.Infrastructure.Context;
+using AnimalManager.Infrastructure.Repositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IAnimalService, AnimalService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<AnimalDtoValidator>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
